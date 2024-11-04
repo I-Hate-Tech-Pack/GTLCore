115666f1fd685d66f0f0d4f0e49a034d5192f9c9
EasterFG <EasterFG@gmail.com>
2024-11-04 18:47:51 +0800

    build: 更新日志1.0.4-previve

---
+++ src/main/java/org/gtlcore/gtlcore/mixin/gtm/MultiblockInfoCategoryMixin.java
@@ -0,0 +1,25 @@
+package org.gtlcore.gtlcore.mixin.gtm;
+
+import com.gregtechceu.gtceu.integration.jei.multipage.MultiblockInfoCategory;
+
+import com.lowdragmc.lowdraglib.Platform;
+
+import mezz.jei.api.registration.IRecipeRegistration;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * @author EasterFG on 2024/10/28
+ */
+@Mixin(MultiblockInfoCategory.class)
+public class MultiblockInfoCategoryMixin {
+
+    @Inject(method = "registerRecipes", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void registerRecipes(IRecipeRegistration registry, CallbackInfo ci) {
+        if (Platform.isDevEnv()) {
+            ci.cancel();
+        }
+    }
+}
