2cbb87d2667b67ff10ad71228a1c1a15bc958e76
wanggugu <87158048+wanggugu197@users.noreply.github.com>
2025-06-03 13:35:02 +0800

    fix

--- src/main/java/org/gtlcore/gtlcore/GTLGTAddon.java
+++ src/main/java/org/gtlcore/gtlcore/GTLGTAddon.java
@@ -3,7 +3,6 @@ package org.gtlcore.gtlcore;
 import org.gtlcore.gtlcore.api.data.tag.GTLTagPrefix;
 import org.gtlcore.gtlcore.api.registries.GTLRegistration;
 import org.gtlcore.gtlcore.common.data.*;
-import org.gtlcore.gtlcore.config.ConfigHolder;
 import org.gtlcore.gtlcore.data.recipe.*;
 import org.gtlcore.gtlcore.data.recipe.chemistry.MixerRecipes;
 import org.gtlcore.gtlcore.data.recipe.processing.Lanthanidetreatment;
@@ -76,9 +75,7 @@ public class GTLGTAddon implements IGTAddon {
         Lanthanidetreatment.init(provider);
         CircuitRecipes.init(provider);
         MixerRecipes.init(provider);
-        if (ConfigHolder.INSTANCE.enableSkyBlokeMode) {
-            SkyTearsAndGregHeart.init(provider);
-        }
+        SkyTearsAndGregHeart.init(provider);
     }
 
     @Override
