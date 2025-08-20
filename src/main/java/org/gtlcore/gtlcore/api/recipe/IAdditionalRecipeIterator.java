f069c658fb09962c10b61f3ade7d3f93209c9c90
Dragonators <13391169509@163.com>
2025-08-20 23:58:40 +0800

    修复当样板槽内有物品时取出催化剂或修改电路，GTRecipe缓存未经canhandle判断便进入迭代器导致无限重复尝试的问题;去除lastFailedMatches操作;修复样板总成镜像在不同种类机器上时仍能运行配方的问题

--- src/main/java/org/gtlcore/gtlcore/api/recipe/IAdditionalRecipeIterator.java
+++ src/main/java/org/gtlcore/gtlcore/api/recipe/IAdditionalRecipeIterator.java
@@ -6,10 +6,15 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public interface IAdditionalRecipeIterator {
 
     void setAdditionalRecipes(@NotNull List<@NotNull GTRecipe> additionalRecipes);
+
+    void setAdditionalRecipesCanHandle(@NotNull Predicate<GTRecipe> canHandle);
+
+    boolean hasAdditionalRecipes();
 
     @Nullable
     List<GTRecipe> getAdditionalRecipes();
