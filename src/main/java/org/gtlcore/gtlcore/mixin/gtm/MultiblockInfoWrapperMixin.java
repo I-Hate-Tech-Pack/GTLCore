a4826cff4d3532b8eca6152915a88beba6717414
AaAdoniSsS <3427996621@qq.com>
2025-09-04 19:17:14 +0800

    jei结构预览优化

---
+++ src/main/java/org/gtlcore/gtlcore/mixin/gtm/MultiblockInfoWrapperMixin.java
@@ -0,0 +1,24 @@
+package org.gtlcore.gtlcore.mixin.gtm;
+
+import org.gtlcore.gtlcore.api.gui.PatternPreviewWidget;
+
+import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
+import com.gregtechceu.gtceu.integration.jei.multipage.MultiblockInfoWrapper;
+
+import com.lowdragmc.lowdraglib.gui.widget.Widget;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(MultiblockInfoWrapper.class)
+public class MultiblockInfoWrapperMixin {
+
+    @ModifyArg(method = "<init>",
+               at = @At(value = "INVOKE",
+                        target = "Lcom/lowdragmc/lowdraglib/jei/ModularWrapper;<init>(Lcom/lowdragmc/lowdraglib/gui/widget/Widget;)V"))
+    private static Widget MultiblockInfoWrapper(Widget par1, @Local(ordinal = 0, argsOnly = true) MultiblockMachineDefinition definition) {
+        return PatternPreviewWidget.getPatternWidget(definition);
+    }
+}
