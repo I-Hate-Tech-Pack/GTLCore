47f2f28e0d9f8946611b70e0c031f56ff4f218e6
AaAdoniSsS <3427996621@qq.com>
2025-09-07 08:07:26 +0800

    优化ae下单计算和发货性能

---
+++ src/main/java/org/gtlcore/gtlcore/mixin/ae2/crafting/CraftingCpuHelperMixin.java
@@ -0,0 +1,96 @@
+package org.gtlcore.gtlcore.mixin.ae2.crafting;
+
+import net.minecraft.world.level.Level;
+
+import appeng.api.crafting.IPatternDetails;
+import appeng.api.stacks.*;
+import appeng.crafting.execution.CraftingCpuHelper;
+import appeng.crafting.execution.InputTemplate;
+import appeng.crafting.inv.ICraftingInventory;
+import appeng.crafting.pattern.AEProcessingPattern;
+import com.google.common.collect.Iterables;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+import static appeng.crafting.execution.CraftingCpuHelper.extractTemplates;
+import static appeng.crafting.execution.CraftingCpuHelper.reinjectPatternInputs;
+
+@Mixin(CraftingCpuHelper.class)
+public class CraftingCpuHelperMixin {
+
+    @Inject(method = "extractPatternInputs", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void extractPatternInputs(IPatternDetails details,
+                                             ICraftingInventory sourceInv,
+                                             Level level,
+                                             KeyCounter expectedOutputs,
+                                             CallbackInfoReturnable<KeyCounter[]> cir) {
+        if (details instanceof AEProcessingPattern) {
+            IPatternDetails.IInput[] inputs = details.getInputs();
+            KeyCounter[] inputHolder = new KeyCounter[inputs.length];
+            boolean found = true;
+
+            for (int x = 0; x < inputs.length; x++) {
+                var list = inputHolder[x] = new KeyCounter();
+                long remainingMultiplier = inputs[x].getMultiplier();
+                for (var template : getValidItemTemplate(inputs[x], level)) {
+                    long extracted = extractTemplates(sourceInv, template, remainingMultiplier);
+                    list.add(template.key(), extracted * template.amount());
+
+                    var containerItem = inputs[x].getRemainingKey(template.key());
+                    if (containerItem != null) {
+                        expectedOutputs.add(containerItem, extracted);
+                    }
+
+                    remainingMultiplier -= extracted;
+                    if (remainingMultiplier == 0)
+                        break;
+                }
+
+                if (remainingMultiplier > 0) {
+                    found = false;
+                    break;
+                }
+            }
+
+            if (!found) {
+                reinjectPatternInputs(sourceInv, inputHolder);
+                cir.setReturnValue(null);
+            } else {
+                for (var output : details.getOutputs()) expectedOutputs.add(output.what(), output.amount());
+                cir.setReturnValue(inputHolder);
+            }
+        }
+    }
+
+    /**
+     * @author .
+     * @reason 提升性能
+     */
+    @Overwrite(remap = false)
+    public static Iterable<InputTemplate> getValidItemTemplates(ICraftingInventory inv,
+                                                                IPatternDetails.IInput input, Level level) {
+        var possibleInputs = input.getPossibleInputs();
+        List<InputTemplate> substitutes = new ObjectArrayList<>(possibleInputs.length);
+        for (var stack : possibleInputs) {
+            for (var fuzz : inv.findFuzzyTemplates(stack.what())) {
+                if (fuzz instanceof AEItemKey aeItemKey && !aeItemKey.matches(stack)) continue;
+                else if (fuzz instanceof AEFluidKey aeFluidKey && !aeFluidKey.matches(stack)) continue;
+                substitutes.add(new InputTemplate(fuzz, stack.amount()));
+            }
+        }
+        return Iterables.filter(substitutes, stack -> input.isValid(stack.key(), level));
+    }
+
+    @Unique
+    private static Iterable<InputTemplate> getValidItemTemplate(IPatternDetails.IInput input, Level level) {
+        var possibleInputs = input.getPossibleInputs();
+        List<InputTemplate> substitutes = new ObjectArrayList<>(possibleInputs.length);
+        for (var stack : possibleInputs) substitutes.add(new InputTemplate(stack.what(), stack.amount()));
+        return Iterables.filter(substitutes, stack -> input.isValid(stack.key(), level));
+    }
+}
---
+++ src/main/java/org/gtlcore/gtlcore/mixin/ae2/crafting/CraftingTreeNodeMixin.java
@@ -0,0 +1,67 @@
+package org.gtlcore.gtlcore.mixin.ae2.crafting;
+
+import net.minecraft.world.level.Level;
+
+import appeng.api.crafting.IPatternDetails;
+import appeng.api.networking.crafting.ICraftingService;
+import appeng.api.stacks.*;
+import appeng.crafting.*;
+import appeng.crafting.execution.CraftingCpuHelper;
+import appeng.crafting.execution.InputTemplate;
+import appeng.crafting.inv.ICraftingInventory;
+import appeng.crafting.pattern.AEProcessingPattern;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(CraftingTreeNode.class)
+public class CraftingTreeNodeMixin {
+
+    @Unique
+    private IPatternDetails patternDetails;
+
+    @Shadow(remap = false)
+    @Final
+    @Mutable
+    final IPatternDetails.@Nullable IInput parentInput;
+    @Shadow(remap = false)
+    @Final
+    @Mutable
+    private final Level level;
+    @Shadow(remap = false)
+    @Final
+    @Mutable
+    private final AEKey what;
+
+    public CraftingTreeNodeMixin(IPatternDetails.@Nullable IInput parentInput, Level level, AEKey what) {
+        this.parentInput = parentInput;
+        this.level = level;
+        this.what = what;
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void CraftingTreeNode(ICraftingService cc,
+                                  CraftingCalculation job, AEKey what, long amount,
+                                  CraftingTreeProcess par, int slot, CallbackInfo ci) {
+        this.patternDetails = slot == -1 ? null : ((CraftingTreeProcessAccessor) par).getDetails();
+    }
+
+    /**
+     * @author .
+     * @reason .
+     */
+    @Overwrite(remap = false)
+    private Iterable<InputTemplate> getValidItemTemplates(ICraftingInventory inv) {
+        if (this.parentInput == null)
+            return List.of(new InputTemplate(what, 1));
+        else if (this.patternDetails instanceof AEProcessingPattern) {
+            GenericStack stack = this.parentInput.getPossibleInputs()[0];
+            return List.of(new InputTemplate(stack.what(), stack.amount()));
+        }
+        return CraftingCpuHelper.getValidItemTemplates(inv, this.parentInput, level);
+    }
+}
