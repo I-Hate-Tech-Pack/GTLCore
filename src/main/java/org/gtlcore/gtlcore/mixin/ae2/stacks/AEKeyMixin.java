622ac6725287e868ad0691fa57aca2eb18a9bbca
Dragonators <13391169509@163.com>
2025-09-17 22:09:13 +0800

    优化AEKey toTagGeneric

---
+++ src/main/java/org/gtlcore/gtlcore/mixin/ae2/stacks/AEKeyMixin.java
@@ -0,0 +1,40 @@
+package org.gtlcore.gtlcore.mixin.ae2.stacks;
+
+import net.minecraft.nbt.CompoundTag;
+
+import appeng.api.stacks.AEKey;
+import appeng.api.stacks.AEKeyType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(AEKey.class)
+public abstract class AEKeyMixin {
+
+    @Unique
+    private CompoundTag gTLCore$tagGenericCache;
+
+    @Shadow(remap = false)
+    public abstract CompoundTag toTag();
+
+    @Shadow(remap = false)
+    public abstract AEKeyType getType();
+
+    /**
+     * @author Dragons
+     * @reason Performance
+     */
+    @Overwrite(remap = false)
+    public final CompoundTag toTagGeneric() {
+        return gTLCore$tagGenericCache != null ? gTLCore$tagGenericCache : gTLCore$saveAndReturnTagGeneric();
+    }
+
+    @Unique
+    private CompoundTag gTLCore$saveAndReturnTagGeneric() {
+        CompoundTag tag = this.toTag();
+        tag.putString("#c", this.getType().getId().toString());
+        this.gTLCore$tagGenericCache = tag;
+        return tag;
+    }
+}
