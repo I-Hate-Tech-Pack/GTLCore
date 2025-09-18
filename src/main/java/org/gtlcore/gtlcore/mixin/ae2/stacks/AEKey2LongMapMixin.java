984108ec4a481d5fb11671fed3f316499b88a29f
AaAdoniSsS <3427996621@qq.com>
2025-09-18 23:43:32 +0800

    Revert "fix"
    This reverts commit b34ecc7cfcb23d43dc6dd47e32dbb6a13763837a.

--- src/main/java/org/gtlcore/gtlcore/mixin/ae2/stacks/AEKey2LongMapMixin.java
+++ src/main/java/org/gtlcore/gtlcore/mixin/ae2/stacks/AEKey2LongMapMixin.java
@@ -1,40 +1,21 @@
 package org.gtlcore.gtlcore.mixin.ae2.stacks;
 
 import appeng.api.stacks.AEKey;
-import it.unimi.dsi.fastutil.HashCommon;
 import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
 import org.spongepowered.asm.mixin.Mixin;
-
-import static it.unimi.dsi.fastutil.HashCommon.arraySize;
 
 @Mixin(targets = "appeng.api.stacks.AEKey2LongMap$OpenHashMap", remap = false)
 public abstract class AEKey2LongMapMixin extends Object2LongOpenHashMap<AEKey> {
 
     @Override
-    public long addTo(AEKey k, long incr) {
-        if (incr == 0) return defRetValue;
-        int pos;
-        if (k == null) {
-            if (containsNullKey) return addToValue(n, incr);
-            pos = n;
-            containsNullKey = true;
-        } else {
-            AEKey curr;
-            var key = this.key;
-            // The starting point.
-            if (!((curr = key[pos = (HashCommon.mix(k.hashCode())) & mask]) == null)) {
-                if (curr.equals(k)) return addToValue(pos, incr);
-                while (!((curr = key[pos = (pos + 1) & mask]) == null)) if (curr.equals(k)) return addToValue(pos, incr);
-            }
-        }
-        key[pos] = k;
-        value[pos] = defRetValue + incr;
-        if (size++ >= maxFill) rehash(arraySize(size + 1, f));
-        return defRetValue;
-    }
+    public long addTo(AEKey key, long incr) {
+        long oldValue = getLong(key);
 
-    private long addToValue(int pos, long incr) {
-        long oldValue = value[pos];
+        // Fast path: zero increment
+        if (incr == 0) {
+            return oldValue;
+        }
+
         long newValue = oldValue + incr;
 
         // Check for overflow using bitwise operations (faster than bounds checking)
@@ -43,7 +24,7 @@ public abstract class AEKey2LongMapMixin extends Object2LongOpenHashMap<AEKey> {
             newValue = incr > 0 ? Long.MAX_VALUE : Long.MIN_VALUE;
         }
 
-        value[pos] = newValue;
+        put(key, newValue);
         return oldValue;
     }
 }
