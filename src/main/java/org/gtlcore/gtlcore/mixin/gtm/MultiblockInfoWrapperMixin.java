75b8cb37c3cf830e64070b629e463fad987f5418
AaAdoniSsS <3427996621@qq.com>
2025-09-19 19:24:32 +0800

    终极终端

--- src/main/java/org/gtlcore/gtlcore/mixin/gtm/MultiblockInfoWrapperMixin.java
+++ src/main/java/org/gtlcore/gtlcore/mixin/gtm/MultiblockInfoWrapperMixin.java
@@ -1,7 +1,6 @@
 package org.gtlcore.gtlcore.mixin.gtm;
 
 import org.gtlcore.gtlcore.api.gui.PatternPreviewWidget;
-import org.gtlcore.gtlcore.common.block.BlockMap;
 
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.integration.jei.multipage.MultiblockInfoWrapper;
@@ -20,7 +19,6 @@ public class MultiblockInfoWrapperMixin {
                at = @At(value = "INVOKE",
                         target = "Lcom/lowdragmc/lowdraglib/jei/ModularWrapper;<init>(Lcom/lowdragmc/lowdraglib/gui/widget/Widget;)V"))
     private static Widget MultiblockInfoWrapper(Widget par1, @Local(ordinal = 0, argsOnly = true) MultiblockMachineDefinition definition) {
-        BlockMap.build();
         return PatternPreviewWidget.getPatternWidget(definition);
     }
 }
