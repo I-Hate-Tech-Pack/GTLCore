137a177ade9141052b4bf51089a2a92ae023d4d5
AaAdoniSsS <3427996621@qq.com>
2025-09-26 17:55:56 +0800

    fix

--- src/main/java/org/gtlcore/gtlcore/api/gui/BlockMapSelectorWidget.java
+++ src/main/java/org/gtlcore/gtlcore/api/gui/BlockMapSelectorWidget.java
@@ -1,43 +1,104 @@
 package org.gtlcore.gtlcore.api.gui;
 
+import org.gtlcore.gtlcore.common.block.BlockMap;
+
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
 
-import com.lowdragmc.lowdraglib.gui.widget.SelectorWidget;
+import com.lowdragmc.lowdraglib.gui.editor.ColorPattern;
+import com.lowdragmc.lowdraglib.gui.texture.ItemStackTexture;
+import com.lowdragmc.lowdraglib.gui.widget.*;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 
 import java.util.*;
+import java.util.function.*;
 
-public class BlockMapSelectorWidget extends SelectorWidget {
+import static net.minecraft.network.chat.Component.translatable;
 
-    public BlockMapSelectorWidget(int x, int y, int width, int height, List<Block> blocks) {
-        super(x, y, width, height, blocks.isEmpty() ? List.of() :
-                blocks.stream().map(b -> b.getName().getString()).toList(), -1);
-        this.setBackground(GuiTextures.BACKGROUND_INVERSE);
-        this.setButtonBackground(GuiTextures.BUTTON);
-        this.textTexture.setRollSpeed(.25f);
-        if (!this.candidates.isEmpty()) this.popUp.setSizeWidth(this.candidates.stream().mapToInt(String::length).max().orElse(0));
-        if (blocks.isEmpty()) this.setVisible(false);
+public class BlockMapSelectorWidget extends WidgetGroup {
+
+    public static final Component SC = translatable("gui.gtlcore.stellar_thermal_container");
+    public static final Component SEPM = translatable("gui.gtlcore.space_elevator_module");
+    public static final Component CAL = translatable("gui.gtlcore.component_assembly_casing");
+    public static final Component COIL = translatable("gui.gtlcore.coil");
+
+    private final BiConsumer<String, Integer> onChanged;
+    private List<Block> blocks;
+    private String currentType;
+
+    public BlockMapSelectorWidget(int y, int width, BiConsumer<String, Integer> onChanged) {
+        super(0, y, width, 56);
+        this.onChanged = onChanged;
+        this.setVisible(false);
     }
 
-    public int getIndex(String s) {
-        return this.candidates.indexOf(s);
+    public static Component getBlock(String string) {
+        return switch (string) {
+            case "sc" -> SC;
+            case "sepm" -> SEPM;
+            case "cal" -> CAL;
+            case "coil" -> COIL;
+            default -> throw new IllegalStateException("Unexpected value: " + string);
+        };
     }
 
-    public void setIndex(int index, List<Block> blocks) {
-        this.setBlocks(blocks);
-        if (index < 0 || index >= this.candidates.size()) return;
-        this.setValue(this.candidates.get(index));
-        this.setVisible(true);
+    public void setInit(ItemStack itemStack) {
+        var tag = itemStack.getOrCreateTag();
+        var block = tag.getString("blocks");
+        if (!block.isEmpty()) {
+            this.blocks = Arrays.stream(BlockMap.tierBlockMap.get(block).get()).toList();
+            this.currentType = block;
+        }
     }
 
-    public void setBlocks(List<Block> blocks) {
-        boolean isEmpty = blocks.isEmpty();
-        this.setCandidates(isEmpty ? List.of() : blocks.stream().map(b -> b.getName().getString()).toList());
+    public void showType(boolean isShow) {
+        if (!isShow) this.setVisible(false);
+        else {
+            WidgetGroup group = new WidgetGroup(0, 2, 80, 52);
+            group.setBackground(GuiTextures.BACKGROUND_INVERSE);
+            var blockType = new DraggableScrollableWidgetGroup(4, 4, 72, 44);
+            blockType.setYScrollBarWidth(2)
+                    .setYBarStyle(null, ColorPattern.T_WHITE.rectTexture().setRadius(1.0F))
+                    .setBackground(GuiTextures.DISPLAY);
+            int y = 0;
+            for (var key : BlockMap.tierBlockMap.keySet()) {
+                blockType.addWidget((new WidgetGroup(2, 2 + y, 66, 15))
+                        .addWidget(new ExtendLabelWidget(0, 0, getBlock(key)))
+                        .addWidget(new ButtonWidget(0, 0, 64, 15, (cd) -> {
+                            showTier(Arrays.stream(BlockMap.tierBlockMap.get(key).get()).toList());
+                            currentType = key;
+                        })));
+                y += 15;
+            }
+            this.addWidget(group.addWidget(blockType));
+            this.setVisible(true);
+            if (this.blocks != null && !this.blocks.isEmpty()) showTier(this.blocks);
+        }
     }
 
-    public void isShow() {
-        this.setVisible(true);
-        this.setShow(true);
+    public void showTier(List<Block> blocks) {
+        if (!blocks.isEmpty()) {
+            this.blocks = blocks;
+            WidgetGroup group = new WidgetGroup(80, 2, 120, 52);
+            group.setBackground(GuiTextures.BACKGROUND_INVERSE);
+            var blockTier = new DraggableScrollableWidgetGroup(4, 4, 112, 44);
+            blockTier.setYScrollBarWidth(2)
+                    .setYBarStyle(null, ColorPattern.T_WHITE.rectTexture().setRadius(1.0F))
+                    .setBackground(GuiTextures.DISPLAY);
+            for (int i = 0; i < blocks.size(); i++) {
+                int finalI = i;
+                var block = blocks.get(finalI);
+                blockTier.addWidget(new WidgetGroup(2, 2 + i * 20, 110, 22)
+                        .addWidget(new ImageWidget(2, 0, 18, 18,
+                                new ItemStackTexture(block.asItem().getDefaultInstance())))
+                        .addWidget(new ExtendLabelWidget(20, 4, block.getName()))
+                        .addWidget(new ButtonWidget(20, 0, 88, 18, (cd) -> {
+                            if (onChanged != null) onChanged.accept(currentType, finalI);
+                        })));
+            }
+            this.addWidget(group.addWidget(blockTier));
+        }
     }
 }
