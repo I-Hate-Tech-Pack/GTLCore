7bdd9f8696a01ce261fab949aa1f25fc0754bbc1
AaAdoniSsS <3427996621@qq.com>
2025-10-02 09:55:44 +0800

    fix

--- src/main/java/org/gtlcore/gtlcore/mixin/ae2/gui/SetProcessingPatternAmountScreenMixin.java
+++ src/main/java/org/gtlcore/gtlcore/mixin/ae2/gui/SetProcessingPatternAmountScreenMixin.java
@@ -1,11 +1,15 @@
 package org.gtlcore.gtlcore.mixin.ae2.gui;
 
+import appeng.api.stacks.GenericStack;
 import appeng.client.gui.me.items.SetProcessingPatternAmountScreen;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.*;
 
 @Mixin(SetProcessingPatternAmountScreen.class)
 public class SetProcessingPatternAmountScreenMixin {
+
+    @Shadow(remap = false)
+    @Final
+    private GenericStack currentStack;
 
     /**
      * @author .
@@ -13,6 +17,6 @@ public class SetProcessingPatternAmountScreenMixin {
      */
     @Overwrite(remap = false)
     private long getMaxAmount() {
-        return Integer.MAX_VALUE;
+        return (long) Integer.MAX_VALUE * this.currentStack.what().getAmountPerUnit();
     }
 }
