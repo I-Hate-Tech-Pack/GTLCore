bb21d02d2067252a9644cfa26c4202de930cb1f4
Dragonators <13391169509@163.com>
2025-10-04 18:08:24 +0800

    修复热交换机使用样板总成时丢失并行的问题

--- src/main/java/org/gtlcore/gtlcore/common/machine/multiblock/noenergy/HeatExchangerMachine.java
+++ src/main/java/org/gtlcore/gtlcore/common/machine/multiblock/noenergy/HeatExchangerMachine.java
@@ -3,7 +3,7 @@ package org.gtlcore.gtlcore.common.machine.multiblock.noenergy;
 import org.gtlcore.gtlcore.GTLCore;
 import org.gtlcore.gtlcore.api.machine.multiblock.NoEnergyMultiblockMachine;
 import org.gtlcore.gtlcore.api.recipe.RecipeResult;
-import org.gtlcore.gtlcore.common.data.GTLMaterials;
+import org.gtlcore.gtlcore.api.recipe.RecipeRunnerHelper;
 import org.gtlcore.gtlcore.utils.MachineIO;
 
 import com.gregtechceu.gtceu.api.capability.recipe.FluidRecipeCapability;
@@ -11,6 +11,7 @@ import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.feature.IRecipeLogicMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
+import com.gregtechceu.gtceu.api.recipe.ingredient.FluidIngredient;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
@@ -27,36 +28,32 @@ public class HeatExchangerMachine extends NoEnergyMultiblockMachine {
         super(holder, args);
     }
 
-    private long count = 0;
-
     public static GTRecipe recipeModifier(MetaMachine machine, @NotNull GTRecipe recipe) {
         if (machine instanceof HeatExchangerMachine hMachine) {
             if (FluidRecipeCapability.CAP.of(recipe.inputs.get(FluidRecipeCapability.CAP)
                     .get(1).getContent()).getStacks()[0].getFluid() == GTMaterials.Water.getFluid()) {
                 return GTRecipeModifiers.accurateParallel(machine, recipe, Integer.MAX_VALUE, false).getFirst();
             }
-            final Pair<GTRecipe, Integer> result = GTRecipeModifiers.accurateParallel(machine, new GTRecipeBuilder(GTLCore.id("heat_exchanger"), GTRecipeTypes.DUMMY_RECIPES)
+
+            // for recipe cache
+            final GTRecipe plasmaRecipe = new GTRecipeBuilder(GTLCore.id("heat_exchanger"), GTRecipeTypes.DUMMY_RECIPES)
                     .inputFluids(FluidRecipeCapability.CAP.of(recipe.inputs
                             .get(FluidRecipeCapability.CAP).get(0).getContent()))
-                    .outputFluids(FluidRecipeCapability.CAP.of(recipe.outputs
-                            .get(FluidRecipeCapability.CAP).get(0).getContent()))
+                    .outputFluids(recipe.outputs.get(FluidRecipeCapability.CAP).stream().map(c -> FluidRecipeCapability.CAP.of(c.getContent())).toArray((FluidIngredient[]::new)))
                     .duration(200)
-                    .buildRawRecipe(), Integer.MAX_VALUE, false);
-            long count = result.getSecond() * recipe.data.getLong("eu");
-            if (MachineIO.inputFluid(hMachine, GTMaterials.DistilledWater.getFluid(count / 160))) {
-                hMachine.count = count;
+                    .buildRawRecipe();
+
+            if (!RecipeRunnerHelper.matchRecipe(hMachine, plasmaRecipe)) return null;
+
+            final Pair<GTRecipe, Integer> result = GTRecipeModifiers.accurateParallel(machine, plasmaRecipe, Integer.MAX_VALUE, false);
+
+            long count = result.getSecond() * FluidRecipeCapability.CAP.of(recipe.inputs.get(FluidRecipeCapability.CAP)
+                    .get(1).getContent()).getStacks()[0].getAmount();
+
+            if (MachineIO.inputFluid(hMachine, GTMaterials.DistilledWater.getFluid(count))) {
                 return result.getFirst();
             } else RecipeResult.of((IRecipeLogicMachine) machine, RecipeResult.fail(Component.translatable("gtceu.recipe.fail.no.enough.distilledwater")));
         }
         return null;
-    }
-
-    @Override
-    public void afterWorking() {
-        if (count != 0) {
-            MachineIO.outputFluid(this, GTLMaterials.SupercriticalSteam
-                    .getFluid(count));
-        }
-        count = 0;
     }
 }
