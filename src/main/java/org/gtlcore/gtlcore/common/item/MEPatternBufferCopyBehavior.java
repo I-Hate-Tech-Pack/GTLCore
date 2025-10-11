8a922220736e97d7f55793e6e2c1566926d899f8
Dragonators <13391169509@163.com>
2025-10-11 23:43:11 +0800

    样板总成复制工具API迁移,增加复制样板数量设定项;修复放置相同样板再取出其中一个时AE网络无法获取样板的问题

--- src/main/java/org/gtlcore/gtlcore/common/item/MEPatternBufferCopyBehavior.java
+++ src/main/java/org/gtlcore/gtlcore/common/item/MEPatternBufferCopyBehavior.java
@@ -6,22 +6,18 @@ import com.gregtechceu.gtceu.api.blockentity.MetaMachineBlockEntity;
 import com.gregtechceu.gtceu.api.item.component.IInteractionItem;
 import com.gregtechceu.gtceu.common.item.TooltipBehavior;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 
-import appeng.core.definitions.AEItems;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.function.Consumer;
 
 import static com.gregtechceu.gtceu.api.item.tool.ToolHelper.getBehaviorsTag;
-import static org.gtlcore.gtlcore.api.pattern.AdvancedBlockPattern.foundItem;
 
 public class MEPatternBufferCopyBehavior extends TooltipBehavior implements IInteractionItem {
 
@@ -42,36 +38,9 @@ public class MEPatternBufferCopyBehavior extends TooltipBehavior implements IInt
                     var tags = getBehaviorsTag(itemStack);
                     if (!serverPlayer.isShiftKeyDown()) {
                         if (tags.isEmpty()) serverPlayer.displayClientMessage(Component.translatable("message.gtlcore.pattern_buffer_not_copied"), true);
-                        else {
-                            var tag = (CompoundTag) tags.get("tag");
-                            partMachine.setCustomName(tag.getString("name"));
-                            var list = tag.getList("patterns", 10);
-                            int index = 0, listIndex = 0;
-                            var inv = partMachine.getTerminalPatternInventory();
-                            while (index < inv.size() && listIndex < list.size()) {
-                                if (inv.getStackInSlot(index) != ItemStack.EMPTY) {
-                                    index++;
-                                    continue;
-                                }
-                                var result = foundItem(serverPlayer,
-                                        List.of(AEItems.BLANK_PATTERN.stack()), AEItems.BLANK_PATTERN.stack()::is);
-                                if (result.getA() == null) break;
-                                var handler = result.getB();
-                                inv.setItemDirect(index, ItemStack.of((CompoundTag) list.get(listIndex)));
-                                if (handler != null) handler.extractItem(result.getC(), 1, false);
-                                index++;
-                                listIndex++;
-                            }
-                        }
+                        else partMachine.copyFromTag(tags.getCompound("tag"), serverPlayer);
                     } else {
-                        var tag = new CompoundTag();
-                        tag.putString("name", partMachine.getCustomName());
-                        var listPattern = new ListTag();
-                        partMachine.getPatternSlotMap().values().forEach((i -> {
-                            listPattern.add(partMachine.getTerminalPatternInventory().getStackInSlot(i).serializeNBT());
-                        }));
-                        tag.put("patterns", listPattern);
-                        tags.put("tag", tag);
+                        tags = partMachine.copyToTag(tags);
                         if (tags.isEmpty()) serverPlayer.displayClientMessage(Component.translatable("message.gtlcore.pattern_buffer_copy_failed"), true);
                         else serverPlayer.displayClientMessage(Component.translatable("message.gtlcore.pattern_buffer_copied"), true);
                     }
