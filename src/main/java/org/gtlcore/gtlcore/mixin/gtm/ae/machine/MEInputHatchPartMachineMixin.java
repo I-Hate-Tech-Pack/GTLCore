86e026d5cc547e5429bd024a4079cf62de0c1a6b
Dragonators <13391169509@163.com>
2025-10-24 21:59:00 +0800

    ME舱室连接至网络时自动Notify

--- src/main/java/org/gtlcore/gtlcore/mixin/gtm/ae/machine/MEInputHatchPartMachineMixin.java
+++ src/main/java/org/gtlcore/gtlcore/mixin/gtm/ae/machine/MEInputHatchPartMachineMixin.java
@@ -2,9 +2,14 @@ package org.gtlcore.gtlcore.mixin.gtm.ae.machine;
 
 import org.gtlcore.gtlcore.api.machine.trait.MEStock.IMEPartMachine;
 
+import com.gregtechceu.gtceu.api.capability.recipe.IO;
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.integration.ae2.machine.MEHatchPartMachine;
 import com.gregtechceu.gtceu.integration.ae2.machine.MEInputHatchPartMachine;
 import com.gregtechceu.gtceu.integration.ae2.slot.ExportOnlyAEFluidList;
 
+import appeng.api.networking.IGridNodeListener;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,10 +17,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MEInputHatchPartMachine.class)
-public class MEInputHatchPartMachineMixin {
+public abstract class MEInputHatchPartMachineMixin extends MEHatchPartMachine {
 
     @Shadow(remap = false)
     protected ExportOnlyAEFluidList aeFluidHandler;
+
+    public MEInputHatchPartMachineMixin(IMachineBlockEntity holder, IO io, Object... args) {
+        super(holder, io, args);
+    }
 
     @Inject(method = "autoIO",
             at = @At(value = "INVOKE",
@@ -26,5 +35,11 @@ public class MEInputHatchPartMachineMixin {
         if (aeFluidHandler instanceof IMEPartMachine machine) {
             machine.setChanged(true);
         }
+    }
+
+    @Override
+    public void onMainNodeStateChanged(IGridNodeListener.@NotNull State reason) {
+        super.onMainNodeStateChanged(reason);
+        if (getMainNode().isOnline()) aeFluidHandler.notifyListeners();
     }
 }
