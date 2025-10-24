11a53497c294292160eba4331348019336464352
Dragonators <13391169509@163.com>
2025-10-24 21:18:56 +0800

    修复概率消耗原料在XEI中的Tooltip

--- src/main/java/org/gtlcore/gtlcore/mixin/gtm/api/capability/ItemRecipeCapabilityMixin.java
+++ src/main/java/org/gtlcore/gtlcore/mixin/gtm/api/capability/ItemRecipeCapabilityMixin.java
@@ -5,29 +5,41 @@ import org.gtlcore.gtlcore.api.recipe.ingredient.LongIngredient;
 
 import com.gregtechceu.gtceu.api.capability.recipe.*;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
+import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
+import com.gregtechceu.gtceu.api.recipe.ResearchData;
 import com.gregtechceu.gtceu.api.recipe.chance.logic.ChanceLogic;
 import com.gregtechceu.gtceu.api.recipe.content.*;
 import com.gregtechceu.gtceu.api.recipe.ingredient.IntProviderIngredient;
 import com.gregtechceu.gtceu.api.recipe.ingredient.SizedIngredient;
+import com.gregtechceu.gtceu.api.recipe.ui.GTRecipeTypeUI;
+import com.gregtechceu.gtceu.common.recipe.condition.ResearchCondition;
 import com.gregtechceu.gtceu.common.valueprovider.*;
-import com.gregtechceu.gtceu.integration.GTRecipeWidget;
+import com.gregtechceu.gtceu.config.ConfigHolder;
+import com.gregtechceu.gtceu.utils.FormattingUtil;
+import com.gregtechceu.gtceu.utils.ResearchManager;
 
 import com.lowdragmc.lowdraglib.gui.widget.SlotWidget;
+import com.lowdragmc.lowdraglib.gui.widget.Widget;
+import com.lowdragmc.lowdraglib.jei.IngredientIO;
+import com.lowdragmc.lowdraglib.side.item.IItemTransfer;
+import com.lowdragmc.lowdraglib.utils.CycleItemStackHandler;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.valueproviders.ConstantFloat;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import com.google.common.primitives.Ints;
-import com.llamalad7.mixinextras.sugar.Local;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnknownNullability;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.Unique;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiConsumer;
 
 import static com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability.CAP;
 
@@ -82,27 +94,87 @@ public class ItemRecipeCapabilityMixin extends RecipeCapability<Ingredient> {
         return LongIngredient.copy(content);
     }
 
-    @ModifyArg(method = "applyWidgetInfo",
-               at = @At(value = "INVOKE",
-                        target = "Lcom/lowdragmc/lowdraglib/gui/widget/SlotWidget;setOnAddedTooltips(Ljava/util/function/BiConsumer;)Lcom/lowdragmc/lowdraglib/gui/widget/SlotWidget;"),
-               remap = false)
-    public BiConsumer<SlotWidget, List<Component>> applyWidgetInfo(BiConsumer onAddedTooltips,
-                                                                   @Local(name = "content") Content content,
-                                                                   @Local(name = "recipe") GTRecipe recipe,
-                                                                   @Local(name = "index") int index,
-                                                                   @Local(name = "io") IO io) {
-        return (w, tooltips) -> {
-            var ingredient = CAP.of(content.content);
-            long amount = 1;
-            if (ingredient instanceof SizedIngredient si) amount = si.getAmount();
-            else if (ingredient instanceof LongIngredient li) amount = li.getActualAmount();
-            tooltips.add(Component.translatable("gtceu.machine.quantum_chest.items_stored")
-                    .withStyle(ChatFormatting.DARK_AQUA)
-                    .append(Component.literal(String.valueOf(amount))));
-            GTRecipeWidget.setConsumedChance(content, ChanceLogic.OR, tooltips);
-            if (this.isTickSlot(index, io, recipe)) {
-                tooltips.add(Component.translatable("gtceu.gui.content.per_tick"));
+    @Override
+    public void applyWidgetInfo(@NotNull Widget widget,
+                                int index,
+                                boolean isXEI,
+                                IO io,
+                                GTRecipeTypeUI.@UnknownNullability("null when storage == null") RecipeHolder recipeHolder,
+                                @NotNull GTRecipeType recipeType,
+                                @UnknownNullability("null when content == null") GTRecipe recipe,
+                                @Nullable Content content,
+                                @Nullable Object storage) {
+        if (widget instanceof SlotWidget slot) {
+            if (storage instanceof IItemTransfer items) {
+                if (index >= 0 && index < items.getSlots()) {
+                    slot.setHandlerSlot(items, index);
+                    slot.setIngredientIO(io == IO.IN ? IngredientIO.INPUT : IngredientIO.OUTPUT);
+                    slot.setCanTakeItems(!isXEI);
+                    slot.setCanPutItems(!isXEI && io.support(IO.IN));
+                }
+                // 1 over container size.
+                // If in a recipe viewer and a research slot can be added, add it.
+                if (isXEI && recipeType.isHasResearchSlot() && index == items.getSlots()) {
+                    if (ConfigHolder.INSTANCE.machines.enableResearch) {
+                        ResearchCondition condition = recipeHolder.conditions().stream()
+                                .filter(ResearchCondition.class::isInstance).findAny()
+                                .map(ResearchCondition.class::cast).orElse(null);
+                        if (condition != null) {
+                            List<ItemStack> dataItems = new ArrayList<>();
+                            for (ResearchData.ResearchEntry entry : condition.data) {
+                                ItemStack dataStick = entry.getDataItem().copy();
+                                ResearchManager.writeResearchToNBT(dataStick.getOrCreateTag(), entry.getResearchId(),
+                                        recipeType);
+                                dataItems.add(dataStick);
+                            }
+                            CycleItemStackHandler handler = new CycleItemStackHandler(List.of(dataItems));
+                            slot.setHandlerSlot(handler, 0);
+                            slot.setIngredientIO(IngredientIO.INPUT);
+                            slot.setCanTakeItems(false);
+                            slot.setCanPutItems(false);
+                        }
+                    }
+                }
             }
-        };
+            if (content != null) {
+                slot.setXEIChance((float) content.chance / content.maxChance);
+                slot.setOnAddedTooltips((w, tooltips) -> {
+                    var ingredient = CAP.of(content.content);
+                    long amount = 1;
+                    if (ingredient instanceof SizedIngredient si) amount = si.getAmount();
+                    else if (ingredient instanceof LongIngredient li) amount = li.getActualAmount();
+                    tooltips.add(Component.translatable("gtceu.machine.quantum_chest.items_stored")
+                            .withStyle(ChatFormatting.DARK_AQUA)
+                            .append(Component.literal(String.valueOf(amount))));
+                    gTLCore$setConsumedChance(content, ChanceLogic.OR, tooltips, io);
+                    if (this.isTickSlot(index, io, recipe)) {
+                        tooltips.add(Component.translatable("gtceu.gui.content.per_tick"));
+                    }
+                });
+            }
+        }
+    }
+
+    @Unique
+    private static void gTLCore$setConsumedChance(Content content, ChanceLogic logic, List<Component> tooltips, IO io) {
+        var chance = content.chance;
+        if (chance < ChanceLogic.getMaxChancedValue()) {
+            if (chance == 0) {
+                tooltips.add(Component.translatable("gtceu.gui.content.chance_0"));
+            } else {
+                float chanceFloat = 100 * (float) content.chance / content.maxChance;
+                if (logic != ChanceLogic.NONE && logic != ChanceLogic.OR) {
+                    tooltips.add(Component.translatable(io == IO.IN ? "gtceu.gui.content.chance_1_logic_in" : "gtceu.gui.content.chance_1_logic",
+                            FormattingUtil.formatNumber2Places(chanceFloat), logic.getTranslation())
+                            .withStyle(ChatFormatting.YELLOW));
+                } else {
+                    tooltips.add(FormattingUtil.formatPercentage2Places(io == IO.IN ? "gtceu.gui.content.chance_1_in" : "gtceu.gui.content.chance_1", chanceFloat));
+                }
+                if (content.tierChanceBoost != 0) {
+                    var formatNumber = content.tierChanceBoost > 0 ? "+" + FormattingUtil.formatNumber2Places(content.tierChanceBoost / 100.0f) : FormattingUtil.formatNumber2Places(content.tierChanceBoost / 100.0f);
+                    tooltips.add(Component.translatable("gtceu.gui.content.tier_boost_fix", formatNumber).withStyle(ChatFormatting.YELLOW));
+                }
+            }
+        }
     }
 }
