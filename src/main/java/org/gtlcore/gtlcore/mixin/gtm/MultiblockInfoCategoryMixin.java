03d8abbfa1f849249a2680004f715fd21d7139e5
Dragonators <13391169509@163.com>
2025-11-20 18:41:29 +0800

    修复LDLib1.0.29+版本结构预览下方方块统计错位，并增加Core对于LDLib版本的要求

--- src/main/java/org/gtlcore/gtlcore/mixin/gtm/MultiblockInfoCategoryMixin.java
+++ src/main/java/org/gtlcore/gtlcore/mixin/gtm/MultiblockInfoCategoryMixin.java
@@ -1,20 +1,36 @@
 package org.gtlcore.gtlcore.mixin.gtm;
 
+import org.gtlcore.gtlcore.integration.jei.SlotRecipeWidget;
+
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
 import com.gregtechceu.gtceu.integration.jei.multipage.MultiblockInfoCategory;
 import com.gregtechceu.gtceu.integration.jei.multipage.MultiblockInfoWrapper;
 
+import com.lowdragmc.lowdraglib.gui.ingredient.IRecipeIngredientSlot;
+import com.lowdragmc.lowdraglib.gui.widget.SlotWidget;
+import com.lowdragmc.lowdraglib.gui.widget.TankWidget;
+import com.lowdragmc.lowdraglib.gui.widget.Widget;
+import com.lowdragmc.lowdraglib.jei.ModularUIRecipeCategory;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.thread.BlockableEventLoop;
 
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.runtime.IClickableIngredient;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static com.gregtechceu.gtceu.integration.jei.multipage.MultiblockInfoCategory.RECIPE_TYPE;
@@ -23,7 +39,7 @@ import static com.gregtechceu.gtceu.integration.jei.multipage.MultiblockInfoCate
  * @author EasterFG on 2024/10/28
  */
 @Mixin(MultiblockInfoCategory.class)
-public abstract class MultiblockInfoCategoryMixin {
+public abstract class MultiblockInfoCategoryMixin extends ModularUIRecipeCategory<MultiblockInfoWrapper> {
 
     @Inject(method = "registerRecipes", at = @At("HEAD"), cancellable = true, remap = false)
     private static void registerRecipes(IRecipeRegistration registry, @NotNull CallbackInfo ci) {
@@ -37,5 +53,48 @@ public abstract class MultiblockInfoCategoryMixin {
                     registry.addRecipes(RECIPE_TYPE, recipes);
                 }, executor);
         ci.cancel();
+    }
+
+    @Override
+    public void setRecipe(@NotNull IRecipeLayoutBuilder builder, MultiblockInfoWrapper wrapper, @NotNull IFocusGroup focuses) {
+        wrapper.setRecipeWidget(0, 0);
+        List<Widget> flatVisibleWidgetCollection = wrapper.modularUI.getFlatWidgetCollection();
+        for (int i = 0; i < flatVisibleWidgetCollection.size(); i++) {
+            var widget = flatVisibleWidgetCollection.get(i);
+            if (widget instanceof IRecipeIngredientSlot slot) {
+                var role = mapToRole(slot.getIngredientIO());
+                if (role == null) {
+                    gTLCore$addJEISlot(builder, slot, RecipeIngredientRole.INPUT, i);
+                    gTLCore$addJEISlot(builder, slot, RecipeIngredientRole.OUTPUT, i);
+                } else {
+                    gTLCore$addJEISlot(builder, slot, role, i);
+                }
+            }
+        }
+    }
+
+    @Unique
+    @SuppressWarnings("removal")
+    private static void gTLCore$addJEISlot(IRecipeLayoutBuilder builder, IRecipeIngredientSlot slot, RecipeIngredientRole role, int index) {
+        var slotName = "slot_" + index;
+        IRecipeSlotBuilder slotBuilder = builder.addSlotToWidget(role, (extrasBuilder, recipe, slots) -> {
+            var jeiSlot = slots.stream().filter(s -> s.getSlotName().map(name -> name.equals(slotName)).orElse(false)).findFirst();
+            jeiSlot.ifPresent(drawable -> extrasBuilder.addWidget(new SlotRecipeWidget(slot, drawable)));
+        });
+        for (Object ingredient : slot.getXEIIngredients()) {
+            if (ingredient instanceof IClickableIngredient clickableIngredient) {
+                var type = clickableIngredient.getIngredientType();
+                var ingredients = clickableIngredient.getIngredient();
+                slotBuilder.addIngredient(type, ingredients);
+            }
+        }
+        slotBuilder.setSlotName(slotName);
+        slotBuilder.addRichTooltipCallback((recipeSlotView, tooltipBuilder) -> {
+            if (slot instanceof SlotWidget slotWidget) {
+                tooltipBuilder.addAll(slotWidget.getAdditionalToolTips(new ArrayList<>()));
+            } else if (slot instanceof TankWidget tankWidget) {
+                tooltipBuilder.addAll(tankWidget.getAdditionalToolTips(new ArrayList<>()));
+            }
+        });
     }
 }
