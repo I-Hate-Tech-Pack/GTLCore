d9569d630e41832e3530775ff5ee851460d7fab9
Dragonators <13391169509@163.com>
2025-12-22 21:05:07 +0800

    修复服务端崩溃

--- src/main/java/org/gtlcore/gtlcore/client/ClientProxy.java
+++ src/main/java/org/gtlcore/gtlcore/client/ClientProxy.java
@@ -1,9 +1,12 @@
 package org.gtlcore.gtlcore.client;
 
+import org.gtlcore.gtlcore.api.event.SourceTooltipRegistrationEvent;
 import org.gtlcore.gtlcore.common.CommonProxy;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @OnlyIn(Dist.CLIENT)
 public class ClientProxy extends CommonProxy {
@@ -15,5 +18,14 @@ public class ClientProxy extends CommonProxy {
 
     public static void init() {
         CraftingUnitModelProvider.initCraftingUnitModels();
+    }
+
+    @Override
+    protected void clientSetup(final FMLClientSetupEvent event) {
+        super.clientSetup(event);
+        event.enqueueWork(() -> {
+            // Fire event to allow mods to register their own source tooltips
+            FMLJavaModLoadingContext.get().getModEventBus().post(new SourceTooltipRegistrationEvent());
+        });
     }
 }
